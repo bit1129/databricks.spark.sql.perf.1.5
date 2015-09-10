fc9f41c84ba5f91edaa31411d2f0c910506f5b38
bit1129 <bit1129@163.com>
2015-09-10 15:01:25 +0800

    Initial commit

---
+++ sql.perf.1.5/src/main/scala/test/QueryTest.scala
@@ -0,0 +1,34 @@
+package test
+
+object QueryTest {
+  def main(args: Array[String]): Unit = {
+    val sqlContext = null
+    import com.databricks.spark.sql.perf.tpcds.Tables
+    val dsdgenDir ="/export/App/TPCDSVersion1.3.1/dbgen2"
+    val scaleFactor = 30
+    val tables = new Tables(sqlContext, dsdgenDir, scaleFactor)
+    val location="hdfs://ns1/tmp/spark_perf"
+    val format= "parquet"
+    val overwrite = true
+    val partitionTables=true
+    val useDoubleForDecimal =true
+    val clusterByPartitionColumns = true
+    val filterOutNullPartitionValues = true
+
+
+    tables.genData(location, format, overwrite, partitionTables, useDoubleForDecimal, clusterByPartitionColumns, filterOutNullPartitionValues)
+
+//    tables.createExternalTables(location, format, databaseName, overwrite)
+
+    tables.createTemporaryTables(location, format)
+    import com.databricks.spark.sql.perf.tpcds.TPCDS
+
+    val tpcds = new TPCDS (sqlContext = sqlContext)
+
+    import com.databricks.spark.sql.perf.tpcds.SimpleQueries
+    val queries =  SimpleQueries.q7Derived
+
+    val experiment = tpcds.runExperiment(queriesToRun =SimpleQueries.q7Derived)
+
+  }
+}
